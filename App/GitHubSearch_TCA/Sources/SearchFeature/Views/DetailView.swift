4dbf74fece4a48cb771996aa38e07c3df9b935f2
j.kwon <j.kwon@fenrir.co.jp>
2022-08-20 12:09:16 +0900

    画面のレイアウト追加

---
+++ App/GitHubSearch_TCA/Sources/SearchFeature/Views/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  
+//
+//  Created by クォン ジュンヒョク on 2022/08/20.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    var url: URL
+    var title: String
+    
+    var body: some View {
+        WebView(url: url)
+            .navigationTitle(title)
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+ 
+    var url: URL
+ 
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        let request = URLRequest(url: url)
+        webView.load(request)
+        return webView
+    }
+ 
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: URL(string: "https://www.example.com")!, title: "Example")
+    }
+}
