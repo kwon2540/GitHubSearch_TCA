6db16b70b2252d8f586f9db188e9a08ef4598712
j.kwon <j.kwon@fenrir.co.jp>
2022-10-01 11:23:51 +0900

    ListViewからGitHubRepositoryModel削除

--- App/GitHubSearch_TCA/Sources/SearchFeature/Views/ListView.swift
+++ App/GitHubSearch_TCA/Sources/SearchFeature/Views/ListView.swift
@@ -105,42 +105,6 @@ private struct ListItem: View {
     }
 }
 
-public struct GitHubRepositoryModel: Decodable, Identifiable {
-    
-    public let id: String
-    public let name: String
-    public let fullName: String
-    public let owner: GitHubRepositoryOwnerModel
-    public let htmlUrl: URL
-    public let description: String?
-    public let language: String?
-    public let stargazersCount: String
-    
-    public init(id: String, name: String, fullName: String, owner: GitHubRepositoryOwnerModel, htmlUrl: URL, description: String?, language: String?, stargazersCount: String) {
-        self.id = id
-        self.name = name
-        self.fullName = fullName
-        self.owner = owner
-        self.htmlUrl = htmlUrl
-        self.description = description
-        self.language = language
-        self.stargazersCount = stargazersCount
-    }
-}
-
-public struct GitHubRepositoryOwnerModel: Decodable {
-    
-    public let id: String
-    public let login: String
-    public let avatarUrl: URL
-    
-    public init(id: String, login: String, avatarUrl: URL) {
-        self.id = id
-        self.login = login
-        self.avatarUrl = avatarUrl
-    }
-}
-
 struct ListView_Previews: PreviewProvider {
     
     static var previews: some View {
