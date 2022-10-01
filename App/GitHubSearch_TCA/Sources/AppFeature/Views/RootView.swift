d8dba9f34fa3953162407f144ec2930949448efa
j.kwon <j.kwon@fenrir.co.jp>
2022-10-01 11:24:30 +0900

    ListViewをRootViewに表示

--- App/GitHubSearch_TCA/Sources/AppFeature/Views/RootView.swift
+++ App/GitHubSearch_TCA/Sources/AppFeature/Views/RootView.swift
@@ -5,19 +5,23 @@
 //  Created by クォン ジュンヒョク on 2022/08/13.
 //
 
+import ComposableArchitecture
 import SwiftUI
+import SearchFeature
 
 public struct RootView: View {
+
     public init() {
     }
 
     public var body: some View {
-        Label("Hello, world!", systemImage: "swift")
+        ListView(store: Store(initialState: .init(), reducer: listReducer, environment: SearchEnvironment()))
     }
 }
 
 struct RootView_Previews: PreviewProvider {
+
     static var previews: some View {
-        RootView()
+        ListView(store: Store(initialState: .init(), reducer: listReducer, environment: SearchEnvironment()))
     }
 }
